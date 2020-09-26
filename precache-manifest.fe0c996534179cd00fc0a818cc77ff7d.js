47cf5bd84ec295c1db988fa21d073cd27f8dc190
최보미 <bomi@macaron.local>
2020-09-27 04:06:14 +0900

    Updates

---
+++ precache-manifest.fe0c996534179cd00fc0a818cc77ff7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b7664f83670814da0b201a04f3d2b55",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "6db293d5a6fa0aafbc1b",
+    "url": "/nwitter/static/css/main.9333dc4c.chunk.css"
+  },
+  {
+    "revision": "b910dea21612492ab128",
+    "url": "/nwitter/static/js/2.0467410d.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/nwitter/static/js/2.0467410d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6db293d5a6fa0aafbc1b",
+    "url": "/nwitter/static/js/main.90a6b099.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
